6a3d923d45fae8448de6425ca5bf98a61d3e2604
Elizabethweatherby <elizabethwarren2792@gmail.com>
2023-09-13 18:43:01 -0500

    added styling and edited songs

--- public/js/Songs.js
+++ public/js/Songs.js
@@ -24,20 +24,42 @@ let fetchSongs = async function (genreSearch) {
     let songList = [];
     songInfo.forEach((song, idx) => {
       // render your song stuff in here...
-   /*   const songDetailRow = `
-		<div>
-			<table>
-				<tbody>
-					<tr>
-						<td data-title=${song.title}>${song.title}</td>
-						<td data-artist=${song.artist.name}>${song.artist.name}</td>
-						<td data-album=${song.album.title}>${song.album.title}</td>
-						<td><button class="savebtn">SAVE</button></td>
-					</tr>
-				</tbody>
-			</table>
-		<div>
-		`; */
+      /*   const songDetailRow = `
+       <div>
+         <table>
+           <tbody>
+             <tr>
+               <td data-title=${song.title}>${song.title}</td>
+               <td data-artist=${song.artist.name}>${song.artist.name}</td>
+               <td data-album=${song.album.title}>${song.album.title}</td>
+               <td><button class="savebtn">SAVE</button></td>
+             </tr>
+           </tbody>
+         </table>
+       <div>
+       `; */
+      let tr = document.createElement('tr');
+      let titleTd = document.createElement('td');
+      let artistTd = document.createElement('td');
+      let albumTd = document.createElement('td');
+      let savebtnTd = document.createElement('td');
+      let savebtnEl = document.createElement('button');
+      titleTd.setAttribute('data-title', song.title)
+      artistTd.setAttribute('data-artist', song.artist.name)
+      albumTd.setAttribute('data-album', song.album.title)
+      titleTd.setAttribute('class', 'songItem')
+      artistTd.setAttribute('class', 'songItem')
+      albumTd.setAttribute('class', 'songItem')
+      savebtnEl.setAttribute('class', 'savebtn')
+      savebtnEl.textContent = 'SAVE';
+      savebtnTd.appendChild(savebtnEl)
+      titleTd.textContent = song.title;
+      artistTd.textContent = song.artist.name;
+      albumTd.textContent = song.album.title;
+      tr.appendChild(titleTd);
+      tr.appendChild(artistTd);
+      tr.appendChild(albumTd);
+      tr.appendChild(savebtnTd);
       const songDetailRow = `
 <tr>
   <td data-title=${song.title}>${song.title}</td>
@@ -46,26 +68,25 @@ let fetchSongs = async function (genreSearch) {
   <td><button class="savebtn">SAVE</button></td>
 </tr>
 `;
-     // console.log(top10songs);
-      songList.push(songDetailRow);
+      // console.log(top10songs);
+      top10songs.appendChild(tr);
     });
 
     console.log("All Songs: ", songList);
-   // top10Container.innerHTML = songList;
-    top10songs.innerHTML = songList;
+    // top10Container.innerHTML = songList;
 
     const saveButtons = document.querySelectorAll('.savebtn');
     saveButtons.forEach((button) => {
       button.addEventListener('click', (event) => {
         console.log("Target: ", event.target);
-   
+
         console.log("Target: ", event.target.parentElement);
-    
+
         console.log("Target: ", event.target.parentElement.previousElementSibling);
         console.log("Target: ", event.target.parentElement.previousElementSibling.value);
 
-       // console.log("Target: ", event.target.parentElement).siblings('data-title');
-       // const title = event.target.getAttribute('data-title');
+        // console.log("Target: ", event.target.parentElement).siblings('data-title');
+        // const title = event.target.getAttribute('data-title');
         const title = event.target.parentElement.previousElementSibling.value
         const artistName = event.target.getAttribute('data-artist');
         const albumTitle = event.target.getAttribute('data-album');
@@ -74,7 +95,7 @@ let fetchSongs = async function (genreSearch) {
         // What should be our ENDPOIHT and HTTP METHOD (Data if needed) 
         fetch('/api/users/addSong', {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json'},
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ title, artistName, albumTitle })
         })
         // saveSong(title, artistName, albumTitle);
